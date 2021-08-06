238e03f3872cfe72a4883df2e5885f00e38ee139
lucio Ticali <info@freewebsolution.it>
2021-08-06 11:16:51 +0200

    Connessione al db e crea tabella con phpPDO

---
+++ app/public/conf/index.php
@@ -0,0 +1,14 @@
+<?php
+$servername ="mysql";
+$username = "tutorial";
+$password = "secret";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=tutorial_db", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
